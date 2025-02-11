870d7c52196cf87ee3273f783a2437bfab1fb8d1
Taisia Simakova <stipaxa@gmail.com>
2025-02-11 11:28:13 +0000

    feat: implement ShippingInfo, Order models and add them to admin panel

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
-from .models import CheckoutConfig
+from .models import CheckoutConfig, ShippingInfo, Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("order_id", "user", "email", "total_price",
+                    "status", "created_at", "stripe_pid")
+    search_fields = ("order_id", "email", "user__username",
+                     "stripe_pid")
+    list_filter = ("status", "created_at")
+    readonly_fields = ("order_id", "order_id", "created_at",
+                       "stripe_pid")
+    list_editable = ("status",)
 
 
 @admin.register(CheckoutConfig)
 class CheckoutConfigAdmin(admin.ModelAdmin):
     list_display = ('id', 'free_delivery_threshold', 'delivery_cost',
                     'stripe_currency')
+
+
+@admin.register(ShippingInfo)
+class ShippingInfoAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "phone_number", "street_address1",
+                    "town_or_city", "country", "postcode")
+    search_fields = ("full_name", "phone_number", "street_address1",
+                     "town_or_city", "country", "postcode")
+    list_filter = ("country", "town_or_city")
--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,6 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class CheckoutConfig(models.Model):
@@ -19,3 +21,60 @@ class CheckoutConfig(models.Model):
             f"{self.free_delivery_threshold:.2f} euro, "
             f"Cost: {self.delivery_cost:.2f} euro, "
             f"Currency: {self.stripe_currency})")
+
+
+class ShippingInfo(models.Model):
+    full_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    street_address1 = models.CharField(max_length=256)
+    street_address2 = models.CharField(max_length=256, blank=True, null=True)
+    town_or_city = models.CharField(max_length=100)
+    county = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return f"Shipping Info for {self.full_name}"
+
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('paid', 'Paid'),
+        ('failed', 'Failed'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled'),
+        ('returned', 'Returned'),
+        ('refunded', 'Refunded'),
+    ]
+
+    order_id = models.UUIDField(default=uuid.uuid4,
+                                editable=False, unique=True)
+    status = models.CharField(max_length=20,
+                              choices=STATUS_CHOICES, default='pending')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL,
+                             null=True, blank=True)
+    email = models.EmailField()
+    shipping_info = models.ForeignKey(ShippingInfo, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    stripe_payment_intent = models.CharField(max_length=255,
+                                             null=True, blank=True,
+                                             unique=True)
+    stripe_pid = models.CharField(max_length=255, null=False,
+                                  blank=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        """Generate a formatted order ID (ORD-XXXX-YYYY) if not already set"""
+        if not self.order_id:
+            uuid_str = str(self.order_id).replace("-", "")
+            # order_id will be formatted as ORD-XXXX-YYYY, where:
+            # XXXX - First 4 characters of the UUID
+            # YYYY - Last 4 characters of the UUID
+            self.order_id = f"ORD-{uuid_str[:4]}-{uuid_str[-4:]}"
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Order {self.order_id} - {self.shipping_info.full_name}"
