9af72b492422207e1e59799a66f88b9ea15cbd57
Taisia Simakova <stipaxa@gmail.com>
2025-02-21 10:30:33 +0000

    feat: add logout page

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,17 @@
-from django.contrib.auth import login, logout
+from django.contrib import messages
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.urls import reverse
 
 
-def register_view(request):
+def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
+            auth_login(request, user)
             messages.success(request, "Account created successfully!")
             return redirect("product_list")
         else:
@@ -20,7 +22,7 @@ def register_view(request):
     return render(request, "accounts/register.html", {"form": form})
 
 
-def login_view(request):
+def login(request):
     if request.user.is_authenticated:
         return redirect("product_list")
 
@@ -28,7 +30,7 @@ def login_view(request):
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(request, user)
+            auth_login(request, user)
             messages.success(request, "Successfully logged in!")
             return redirect("product_list")
         else:
@@ -40,7 +42,15 @@ def login_view(request):
     return render(request, "accounts/login.html", {"form": form})
 
 
-def logout_view(request):
-    logout(request)
-    messages.success(request, "You have been logged out!")
-    return redirect("home")
+def logout(request):
+    next = request.POST.get("next", reverse("home"))
+
+    if not request.user.is_authenticated:
+        return redirect(next)
+
+    if request.method == "POST":
+        auth_logout(request)
+        messages.success(request, "You have been logged out!")
+        return redirect(next)
+
+    return render(request, "accounts/logout.html", {"next": next})
