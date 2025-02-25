c7d5d99d9a866784d872afd7f1b5c0fce6556426
Taisia Simakova <stipaxa@gmail.com>
2025-02-25 10:31:56 +0000

    feat: add password_reset_confirm and password_reset_complete urls
    - Change domain to gerbet-and-co-84c4ca4e68bb.herokuapp.com.
    - Update password_reset_email, convert link to button.

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,7 +14,8 @@ urlpatterns = [
         auth_views.PasswordResetView.as_view(
             form_class=CustomPasswordResetForm,
             template_name="accounts/password_reset_form.html",
-            email_template_name="accounts/emails/password_reset_email.html",
+            email_template_name="accounts/emails/password_reset_email.txt",
+            html_email_template_name="accounts/emails/password_reset_email.html",
             subject_template_name="accounts/emails/password_reset_subject.txt"
         ),
         name="password_reset",
@@ -26,4 +27,16 @@ urlpatterns = [
         ),
         name="password_reset_done",
     ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm"),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html"
+        ),
+        name="password_reset_complete"),
 ]
