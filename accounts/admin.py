75a33fecccf45b971fa1d834df0b22bae1a37066
Taisia Simakova <stipaxa@gmail.com>
2025-03-02 19:10:39 +0000

    feat: implement Bootstrap tabs on Account page
    - For reference: https://getbootstrap.com/docs/5.2/components/navs-tabs/#javascript-behavior
    - Add UserContactInfoAdmin to admin console.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserContactInfo
 
-# Register your models here.
+
+@admin.register(UserContactInfo)
+class UserContactInfoAdmin(admin.ModelAdmin):
+    list_display = ("user", "phone_number", "street_address1", "town_or_city",
+                    "postcode", "country")
+    list_filter = ("country", "town_or_city")
+    search_fields = ("user__username", "user__email", "phone_number",
+                     "street_address1", "postcode")
+    ordering = ("user",)
