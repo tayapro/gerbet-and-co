0589bdfbc84990f30d97fc78b8def679ce212b15
Taisia Simakova <stipaxa@gmail.com>
2025-08-01 18:03:56 +0100

    docs: add Dockerfile and VPS deployment instructions to README

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 source .env
 
-IMAGE_REPO=sti80/gerbet-and-co
-IMAGE_TAG=dev
+IMAGE_REPO=gerbet-and-co
+IMAGE_TAG=1.0.0
 
 docker build . --build-arg DATABASE_URL=$DATABASE_URL -t $IMAGE_REPO:$IMAGE_TAG
